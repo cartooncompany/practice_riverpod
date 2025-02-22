88bc08190d59a8a48ef1edc059e93c5942edd141
taejun <ptj01036251@gmail.com>
2025-02-22 18:53:59 +0900

    StreamProvider 사용해보기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_practice/layout/default_layout.dart';
 import 'package:riverpod_practice/screen/future_provider_screen.dart';
 import 'package:riverpod_practice/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_practice/screen/state_provider_screen.dart';
+import 'package:riverpod_practice/screen/stream_provider_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -42,6 +43,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: const Text('FutureProviderScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const StreamProviderScreen(),
+                ),
+              );
+            },
+            child: const Text('StreamProviderScreen'),
           ),
         ],
       ),
