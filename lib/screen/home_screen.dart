630c99bda685922dc2a2ec47269a0eedaed0d14b
taejun <ptj01036251@gmail.com>
2025-03-05 21:57:44 +0900

    listen 함수 새용해보기

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,6 +3,7 @@ import 'package:riverpod_practice/layout/default_layout.dart';
 import 'package:riverpod_practice/screen/auto_dispose_modifier_screen.dart';
 import 'package:riverpod_practice/screen/family_modifier_screen.dart';
 import 'package:riverpod_practice/screen/future_provider_screen.dart';
+import 'package:riverpod_practice/screen/listen_provider_screen.dart';
 import 'package:riverpod_practice/screen/state_notifier_provider_screen.dart';
 import 'package:riverpod_practice/screen/state_provider_screen.dart';
 import 'package:riverpod_practice/screen/stream_provider_screen.dart';
@@ -75,6 +76,16 @@ class HomeScreen extends StatelessWidget {
               );
             },
             child: const Text('AutoDisposeModifierScreen'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (_) => const ListenProviderScreen(),
+                ),
+              );
+            },
+            child: const Text('ListenProviderScreen'),
           ),
         ],
       ),
