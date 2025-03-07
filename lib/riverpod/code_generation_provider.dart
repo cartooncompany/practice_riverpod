7ff801bf480ce3c0f045e84f75784513ce40bcaa
taejun <ptj01036251@gmail.com>
2025-03-07 18:54:13 +0900

    riverpod v2

---
+++ lib/riverpod/code_generation_provider.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'code_generation_provider.g.dart';
+
+final _testProvider = Provider<String>((ref) => 'Hello Code Generation!');
+
+@riverpod
+String gState(GStateRef ref) {
+  return 'Hello Code Generation!';
+}
+
+@riverpod
+Future<int> gStateFuture(GStateFutureRef ref) async {
+  await Future.delayed(Duration(seconds: 3));
+
+  return 10;
+}
+
+@riverpod
+Future<int> gStateFuture2(GStateFuture2Ref ref) async {
+  await Future.delayed(Duration(seconds: 3));
+
+  return 10;
+}
+
+class Parameter {
+  final int number1;
+  final int number2;
+
+  Parameter({
+    required this.number1,
+    required this.number2,
+  });
+}
+
+final _testFamilyProvider = Provider.family<int, Parameter>(
+  (ref, parameter) => parameter.number1 * parameter.number2,
+);
+
+@riverpod
+int gStateMultiply(
+  GStateMultiplyRef ref, {
+  required int number1,
+  required int number2,
+}) {
+  return number1 * number2;
+}
+
+@riverpod
+class GStateNotifier extends _$GStateNotifier {
+
+  @override
+  int build() {
+    return 0;
+  }
+
+  increment() {
+    state++;
+  }
+
+  decrement() {
+    state--;
+  }
+}
---
+++ lib/screen/code_generation_screen.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_practice/layout/default_layout.dart';
+import 'package:riverpod_practice/riverpod/code_generation_provider.dart';
+
+class CodeGenerationScreen extends ConsumerWidget {
+  const CodeGenerationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    print('build');
+
+    final state1 = ref.watch(gStateProvider);
+    final state2 = ref.watch(gStateFutureProvider);
+    final state3 = ref.watch(gStateFuture2Provider);
+    final state4 = ref.watch(gStateMultiplyProvider(number1: 10, number2: 20));
+
+    return DefaultLayout(
+      title: 'CodeGenerationScreen',
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('state1: $state1'),
+          state2.when(
+            data: (data) {
+              return Text(
+                'state2 $state2',
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+          state3.when(
+            data: (data) {
+              return Text(
+                'state3: $data',
+                textAlign: TextAlign.center,
+              );
+            },
+            error: (err, stack) => Text(
+              err.toString(),
+            ),
+            loading: () => const Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+          Text('State4: $state4'),
+          Consumer(
+            builder: (context, ref, child) {
+              print('builder build');
+              final state5 = ref.watch(gStateNotifierProvider);
+
+              return Row(
+                children: [
+                  Text('State5: $state5'),
+                  if(child != null) child,
+                ],
+              );
+            },
+            child: Text('  hello'),
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(gStateNotifierProvider.notifier).increment();
+                },
+                child: Text('increment'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  ref.read(gStateNotifierProvider.notifier).decrement();
+                },
+                child: Text('decrement'),
+              ),
+            ],
+          ),
+          ElevatedButton(
+            onPressed: () {
+              ref.invalidate(gStateNotifierProvider);
+            },
+            child: Text(
+              'Invalidate',
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _StateFiveWidget extends ConsumerWidget {
+  const _StateFiveWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final state5 = ref.watch(gStateNotifierProvider);
+
+    return Text('State5: $state5');
+  }
+}
+
